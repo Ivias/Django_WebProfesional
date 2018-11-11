90f5732ebcaf01242b54e01a706c86d2dca39eb3
Ivias <ivias.aren@gmail.com>
2018-11-11 12:09:03 +0100

    Model Service OK

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
 from core import views
 
 """
@@ -36,3 +38,8 @@ urlpatterns = [
     #Para la administración
     path('admin/', admin.site.urls),
 ]
+
+#Para poder servir ficheros media en de la bbdd
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
